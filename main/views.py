059e4e737e96dc9cf7c7fbf393f5317896e0cc07
yaena1223 <rewrite_the_stars_@naver.com>
2021-06-19 18:51:59 +0900

    first commit

---
+++ main/views.py
@@ -0,0 +1,29 @@
+from django.shortcuts import render
+
+# Create your views here.
+def showmain(request):
+    return render(request, 'main/mainpage.html')
+
+def academic(request):
+    return render(request, 'main/academic.html')
+
+def art(request):
+    return render(request, 'main/art.html')
+
+def atheletic(request):
+    return render(request, 'main/atheletic.html')
+
+def fresh(request):
+    return render(request, 'main/fresh.html')
+
+def performance(request):
+    return render(request, 'main/performance.html')
+
+def research(request):
+    return render(request, 'main/research.html')
+
+def social(request):
+    return render(request, 'main/social.html')
+
+def volunteer(request):
+    return render(request, 'main/volunteer.html')
