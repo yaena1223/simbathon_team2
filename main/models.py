396aea4581b7a0f79249e7cafbf6de2b71c7f06b
2017112570 <okh9712@gmail.com>
2021-06-21 22:11:31 +0900

    0621

--- main/models.py
+++ main/models.py
@@ -1,13 +1,47 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
-class Post(models.Model):
+# 멋사
+class likelion_Post(models.Model):
     id = models.AutoField(primary_key=True)
     title = models.CharField(max_length=200)
-    writer = models.CharField(max_length=100)
+    writer =models.ForeignKey(User,on_delete=models.CASCADE)
     date = models.DateTimeField()
     body = models.TextField()
     image = models.ImageField(upload_to='post/',blank = True, null = True)
+
+
+    def __str__(self):
+        return self.title
+    
+    def summary(self):
+        return self.body[:30]
+
+# cafein
+class cafein_Post(models.Model):
+    id = models.AutoField(primary_key=True)
+    title = models.CharField(max_length=200)
+    writer =models.ForeignKey(User,on_delete=models.CASCADE)
+    date = models.DateTimeField()
+    body = models.TextField()
+    image = models.ImageField(upload_to='post/',blank = True, null = True)
+
+
+    def __str__(self):
+        return self.title
+    
+    def summary(self):
+        return self.body[:30]
+
+class Post(models.Model):
+    id = models.AutoField(primary_key=True)
+    title = models.CharField(max_length=200)
+    writer =models.ForeignKey(User,on_delete=models.CASCADE)
+    date = models.DateTimeField()
+    body = models.TextField()
+    image = models.ImageField(upload_to='post/',blank = True, null = True)
+
 
     def __str__(self):
         return self.title
--- simba/settings.py
+++ simba/settings.py
@@ -39,6 +39,11 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'main',
+    'django.contrib.sites',
+    'allauth',
+    'allauth.account',
+    'allauth.socialaccount',
+    'users'
 ]
 
 MIDDLEWARE = [
@@ -135,3 +140,14 @@ STATIC_ROOT = os.path.join(BASE_DIR,'static')
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 
 MEDIA_URL = '/media/'
+
+AUTHENTICATION_BACKENDS = [ 
+    'django.contrib.auth.backends.ModelBackend',
+    'allauth.account.auth_backends.AuthenticationBackend',
+]
+
+SITE_ID = 1
+
+LOGIN_REDIRECT_URL = '/'
+
+DEFAULT_AUTO_FIELD = 'django.db.models.AutoField'
